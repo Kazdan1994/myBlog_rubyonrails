ca644ac14b3362c7b4166133a61d6f60a304b8d7
Arthur <arthurjacquemin2@gmail.com>
2015-12-02 11:01:28 +0100

    Ajout des vues, controlleurs, routes, models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'articles#index'
+
+  get '/articles/:id' => 'articles#show', as: :article
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,11 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   cities = City.create([{ name: "Chicago" }, { name: "Copenhagen" }])
+#   Mayor.create(name: "Emanuel", city: cities.first)
+
+article1 = Article.create(title: "Un blog avec Ruby on Rails", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium debitis, delectus hic ipsum iure labore laboriosam laborum maiores neque nihil pariatur possimus provident quod, reprehenderit similique velit veniam voluptatem?")
+
+#article2 = Article.create(title: "Ruby on Rails", content: "Web development hurt Ruby on Rails is an open-source web framework thats optimized for programmer happiness and sustainable productivity. It lets you write beautiful code by favoring convention over configuration.")
+
+#article3 = Article.create(title: "blog", content: "A blog is your best bet for a voice among the online crowd. It is a personal website, packed with features, as easy to use as your e-mail.")
