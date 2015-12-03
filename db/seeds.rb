0d12682a5602176576319eb08672b7269f3600ee
Kazdan1994 <arthurjacquemin2@gmail.com>
2015-12-03 15:26:08 +0100

    Ajout des données dans le fichier seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,96 @@ article2 = Article.create(title: "Ruby on Rails", content: "Web development hurt
 
 article3 = Article.create(title: "blog", content: "A blog is your best bet for a voice among the online crowd. It is a personal website, packed with features, as easy to use as your e-mail.")
 
+article4	=	Article.create(title: "amet ultricies sem magna nec",	content: "sit amet massa.")
+
+article5	=	Article.create(title: "egestas hendrerit neque. In ornare",	content: "Morbi metus.")
+
+article6	=	Article.create(title: "egestas. Aliquam fringilla cursus purus.",	content: "Aliquam erat volutpat. Nulla")
+
+article7	=	Article.create(title: "egestas. Sed pharetra, felis eget",	content: "elit sed")
+
+article8	=	Article.create(title: "ridiculus mus. Proin vel nisl.",	content: "orci. Phasellus dapibus quam quis diam. Pellentesque habitant morbi")
+
+article9	=	Article.create(title: "Curabitur vel lectus. Cum sociis",	content: "Nunc laoreet lectus")
+
+article10	=	Article.create(title: "interdum enim non nisi. Aenean",	content: "consectetuer ipsum nunc")
+
+article11	=	Article.create(title: "magna. Cras convallis convallis dolor.",	content: "et libero. Proin mi. Aliquam gravida mauris")
+
+article12	=	Article.create(title: "lacus. Quisque imperdiet, erat nonummy",	content: "viverra. Maecenas iaculis aliquet diam. Sed diam")
+
+article13	=	Article.create(title: "massa. Suspendisse eleifend. Cras sed",	content: "blandit. Nam nulla magna, malesuada vel, convallis in, cursus et,")
+
+article14	=	Article.create(title: "Ut semper pretium neque. Morbi",	content: "eget metus. In nec orci. Donec")
+
+article15	=	Article.create(title: "sed, sapien. Nunc pulvinar arcu",	content: "nec quam. Curabitur vel")
+
+article16	=	Article.create(title: "urna, nec luctus felis purus",	content: "Aliquam fringilla cursus")
+
+article17	=	Article.create(title: "et magnis dis parturient montes",	content: "cursus a, enim. Suspendisse aliquet, sem")
+
+article18	=	Article.create(title: "per inceptos hymenaeos. Mauris ut",	content: "euismod et, commodo")
+
+article19	=	Article.create(title: "Pellentesque ut ipsum ac mi",	content: "Nam nulla magna,")
+
+article20	=	Article.create(title: "commodo ipsum. Suspendisse non leo.",	content: "enim mi tempor lorem, eget mollis lectus pede")
+
+article21	=	Article.create(title: "ut, pharetra sed, hendrerit a",	content: "venenatis")
+
+article22	=	Article.create(title: "Sed dictum. Proin eget odio.",	content: "parturient montes, nascetur ridiculus mus. Proin vel")
+
+article23	=	Article.create(title: "eu dui. Cum sociis natoque",	content: "ipsum. Curabitur consequat, lectus sit amet")
+
+article24	=	Article.create(title: "urna suscipit nonummy. Fusce fermentum",	content: "odio. Phasellus at augue id ante dictum cursus. Nunc")
+
+article25	=	Article.create(title: "Sed molestie. Sed id risus",	content: "Donec consectetuer mauris id sapien. Cras dolor dolor, tempus non,")
+
+article26	=	Article.create(title: "gravida. Aliquam tincidunt, nunc ac",	content: "Donec")
+
+article27	=	Article.create(title: "vel, convallis in, cursus et,",	content: "justo. Praesent luctus.")
+
+article28	=	Article.create(title: "lorem, luctus ut, pellentesque eget,",	content: "convallis dolor. Quisque tincidunt pede ac urna. Ut")
+
+article29	=	Article.create(title: "ut, pellentesque eget, dictum placerat,",	content: "est. Mauris eu turpis. Nulla aliquet. Proin velit. Sed")
+
+article30	=	Article.create(title: "ut, sem. Nulla interdum. Curabitur ",	content: "non,")
+
+article31	=	Article.create(title: "erat volutpat. Nulla facilisis. Suspendisse",	content: "Nunc mauris elit, dictum eu, eleifend nec, malesuada ut,")
+
+article32	=	Article.create(title: "dolor. Quisque tincidunt pede ac",	content: "quam quis diam. Pellentesque habitant")
+
+article33	=	Article.create(title: "elit. Aliquam auctor, velit eget",	content: "Duis dignissim tempor arcu. Vestibulum ut")
+
+article34	=	Article.create(title: "dolor dapibus gravida. Aliquam tincidunt,",	content: "eleifend nec, malesuada ut, sem. Nulla interdum. Curabitur")
+
+article35	=	Article.create(title: "eu nibh vulputate mauris sagittis",	content: "erat vitae risus. Duis")
+
+article36	=	Article.create(title: "est. Nunc ullamcorper, velit in",	content: "parturient")
+
+article37	=	Article.create(title: "Curabitur egestas nunc sed libero.",	content: "ac turpis egestas.")
+
+article38	=	Article.create(title: "nec ante. Maecenas mi felis,",	content: "odio. Etiam ligula tortor, dictum eu,")
+
+article39	=	Article.create(title: "non nisi. Aenean eget metus.",	content: "molestie in, tempus eu, ligula. Aenean euismod mauris")
+
+article40	=	Article.create(title: "scelerisque mollis. Phasellus libero mauris,",	content: "amet metus. Aliquam erat")
+
+article41	=	Article.create(title: "lacus pede sagittis augue, eu ",	content: "vitae mauris")
+
+article42	=	Article.create(title: "arcu et pede. Nunc sed	,", content: "Donec tempus, lorem fringilla ornare placerat, orci lacus vestibulum")
+
+article43	=	Article.create(title: "eget, volutpat ornare, facilisis eget,",	content: "ligula. Aliquam erat volutpat. Nulla dignissim.")
+
+article44	=	Article.create(title: "lacus. Aliquam rutrum lorem ac",	content: "sapien. Nunc pulvinar arcu")
+
+article45	=	Article.create(title: "sit amet diam eu dolor",	content: "penatibus et magnis dis")
+
+article46	=	Article.create(title: "sollicitudin orci sem eget massa.",	content: "faucibus. Morbi vehicula. Pellentesque tincidunt tempus")
+
+article47	=	Article.create(title: "vel, faucibus id, libero. Donec",	content: "egestas blandit. Nam nulla magna,")
+
+article48	=	Article.create(title: "non quam. Pellentesque habitant morbi ",	content: "tempus mauris erat eget ipsum. Suspendisse")
+
+article49	=	Article.create(title: "blandit. Nam nulla magna, malesuada",	content: "ipsum")
+
+article50	=	Article.create(title: "a purus. Duis elementum, dui",	content: "ipsum non arcu.")
